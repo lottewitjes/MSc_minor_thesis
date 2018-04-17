7f58ad5606e0dc5d917b89bcf5e1bdcc32c185ee
lottewitjes <lottewitjes@outlook.com>
2018-04-17 15:37:27 +0200

    script to run fastq_dump on a list of accessions stored in a .txt file

---
+++ fastq_dump_batch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/evn/python
+
+"""A Python script to run fastq-dump on a list of accession contained in a txt file
+
+python fastq_dump_batch.py <accessions_file>
+"""
+
+import sys
+import subprocess
+import os.path
+
+__author__ = "Lotte Witjes"
+__email__ = "lottewitjes@outlook.com"
+__version__ = "1.0"
+
+def run_fastq_dump(accession):
+    cmd = "/metagenomics/lottewitjes/sratoolkit.2.9.0-ubuntu64/bin/fastq-dump {}".format(accession)
+    try:
+        results = subprocess.check_call(cmd, shell=True)
+        return results
+    except subprocess.CalledProcessError as err:
+        print err.output
+        sys.exit()
+
+if __name__ == "__main__":
+    accessions_file = sys.argv[1]
+
+    with open(accessions_file, "r") as thefile:
+        for line in thefile:
+            run_fastq_dump(line.strip())
