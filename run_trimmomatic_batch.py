44d14716ecbe4c35d25cf168d7496a7e484b199d
lottewitjes <lottewitjes@outlook.com>
2018-04-18 14:55:49 +0200

    script to run trimmomatic se in batch

---
+++ run_trimmomatic_batch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/evn/python
+
+"""A Python script that runs Trimmomatic.
+
+python run_trimmomatic_batch.py <input_directory> <output_directory>
+
+Keyword arguments:
+- input_directory
+- output_directory
+Returns:
+- filled output_directory containing Trimmomatic results
+"""
+
+import sys
+import subprocess
+import os.path
+
+__author__ = "Lotte Witjes"
+__email__ = "lottewitjes@outlook.com"
+__date__ = "18th of April 2018"
+__version__ = "1.0"
+
+def run_diamond(file, output_directory):
+    """A function that runs Trimmomatic  on the input files.
+    """
+    file_output = file.split(".")[0] + "_trimmed.fastq"
+    cmd = "java -jar /metagenomics/lottewitjes/programs/Trimmomatic-0.36/trimmomatic-0.36.jar SE -phred33 {} {} ILLUMINACLIP:TruSeq3-SE:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36".format(file, file_output)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    else:
+        try:
+            results = subprocess.check_call(cmd, shell=True)
+            return results
+        except subprocess.CalledProcessError as err:
+            print err.output
+            sys.exit()
+
+if __name__ == "__main__":
+    input_directory = argv[1]
+    output_directory = argv[2]
+
+    file_list = os.listdir(input_directory)
+    for file in file_list:
+        if not file.startswith("NG-5593_4"):
+            run_trimmomatic(file, output_directory)
+
