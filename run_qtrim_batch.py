7bd6990ae1f901fca449ecf9f527f55c2dbfa586
lottewitjes <lottewitjes@outlook.com>
2018-04-19 12:11:56 +0200

    bugfixes

--- run_qtrim_batch.py
+++ run_qtrim_batch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/evn/python
 
 """A Python script that runs Qtrim.
-python run_diamond.py <input_directory> <output_directory>
+python run_qtrim_batch.py <input_directory> <output_directory>
 Keyword arguments:
 - input_director
 - output_directory
@@ -19,24 +19,24 @@ __date__ = "18th of April 2018"
 __version__ = "1.0"
 
 def run_qtrim(file, output_directory):
-    """A function that runs DIAMOND on the input files.
+    """A function that runs Qtrim on the input files.
     """
     file_output = str(output_directory) + str(file.split(".")[0]) + "_trimmed.fastq"
     cmd = "/metagenomics/lottewitjes/programs/QTrim_v1_1/QTrim_v1_1 -fastq {} -o {}".format(file, file_output)
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-    else:
-        try:
-            results = subprocess.check_call(cmd, shell=True)
-            return results
-        except subprocess.CalledProcessError as err:
-            print err.output
-            sys.exit()
+    try:
+        results = subprocess.check_call(cmd, shell=True)
+        return results
+    except subprocess.CalledProcessError as err:
+        print err.output
+        sys.exit()
 
 if __name__ == "__main__":
-    input_directory = argv[1]
-    output_directory = argv[2]
+    input_directory = sys.argv[1]
+    output_directory = sys.argv[2]
 
     filelist = os.listdir(input_directory)
     for file in filelist:
-        run_qtrim(file, output_directory)
+        if not file.startswith("fastqc"):
+            run_qtrim(file, output_directory)
