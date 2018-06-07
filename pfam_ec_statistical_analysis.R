58a3c8cd05f01089b011979bfcc031c6831a26a4
Lotte Witjes <lottewitjes@outlook.com>
2018-06-07 14:28:57 +0200

    more documentation

--- pfam_ec_statistical_analysis.R
+++ pfam_ec_statistical_analysis.R
@@ -65,8 +65,11 @@ subject = c("1 male", "2 female", "1 male", "1 male", "1 male", "1 male", "2 fem
 
 #Transform data into matrices
 pfam_matrix_count = as.matrix(acast(pfam_count, Sample~Pfam, value.var="Count", fill=0))
-
 ec_matrix_count = as.matrix(acast(ec_count, Sample~EC, value.var="Count", fill=0))
+
+#Scale the matrices
+pfam_matrix_count = scale(pfam_matrix_count, center=FALSE, scale=TRUE)
+ec_matrix_count = scale(ec_matrix_count, center=FALSE, scale=TRUE)
 
 #Domain/enzyme alpha/beta/gamma diversity#######################################################################################################################
 beta_diversity_pfam = diversity(pfam_matrix_count, index="shannon")
@@ -164,8 +167,8 @@ richness_ec_plot = ggplot(data = diversity_df_ec, aes(x=timepoints, y=richness_e
 richness_ec_plot
 
 #PCA with count data############################################################################################################################################
-pfam_pca_count = prcomp(pfam_matrix_count, center=FALSE, scale=TRUE)
-ec_pca_count = prcomp(ec_matrix_count, center=FALSE, scale=TRUE)
+pfam_pca_count = prcomp(pfam_matrix_count, center=FALSE, scale=FALSE)
+ec_pca_count = prcomp(ec_matrix_count, center=FALSE, scale=FALSE)
 
 pfam_count_percentage = round(pfam_pca_count$sdev / sum(pfam_pca_count$sdev) * 100, 2)
 ec_count_percentage = round(ec_pca_count$sdev / sum(ec_pca_count$sdev) * 100, 2)
@@ -481,44 +484,3 @@ write.table(colors_unique_enzymome_sub3, file="blastn_metasapp_unique_enzymome_s
 write.table(colors_unique_enzymome_sub4, file="blastn_metasapp_unique_enzymome_sub4_colors.tsv", sep="\t", col.names=FALSE, row.names=FALSE, quote=FALSE)
 
 write.table(colors_core_enzymome, file="blastn_metasapp_core_enzymome_colors.tsv", sep="\t", col.names=FALSE, row.names=FALSE, quote=FALSE)
-
-#Hypergeometric test on KEGG pathways###########################################################################################################################
-setwd("/media/lottewitjes/Lotte Witjes/MSc_minor_thesis/statistical_analysis/")
-
-blastx_kegg_counts = read.table(file="blastx_plots_results/blastx_kegg_enzyme_count.tsv", sep="\t", header=TRUE)
-blastx_kegg_counts = cbind(blastx_kegg_counts, rep(641, length(nrow(blastx_kegg_counts))), rep(6202, length(nrow(blastx_kegg_counts))))
-colnames(blastx_kegg_counts) = c("pathway", "mapped_enzymes", "enzymes_in_pathway", "total_core", "total_ec")
-
-blastn_kegg_counts = read.table(file="blastn_plots_results/blastn_kegg_enzyme_count.tsv", sep="\t", header=TRUE)
-blastn_kegg_counts = cbind(blastn_kegg_counts, rep(924, length(nrow(blastn_kegg_counts))), rep(6202, length(nrow(blastn_kegg_counts))))
-colnames(blastn_kegg_counts) = colnames(blastx_kegg_counts)
-
-blastn_metasapp_kegg_counts = read.table(file="blastn_metasapp_plots_results/blastn_metasapp_kegg_enzyme_count.tsv", sep="\t", header=TRUE)
-blastn_metasapp_kegg_counts = cbind(blastn_metasapp_kegg_counts, rep(1164, length(nrow(blastn_metasapp_kegg_counts))), rep(6202, length(nrow(blastn_metasapp_kegg_counts))))
-colnames(blastn_metasapp_kegg_counts) = colnames(blastx_kegg_counts)
-
-hypgeo_kegg = function(counts) {
-  p_values = c()
-  for (row in 1:nrow(counts)) {
-    if (counts[row,2] > 0) {
-      hypgeo = dhyper(counts[row,2], counts[row,3], (counts[row,5]-counts[row,3]), counts[row,4])
-      p_values[row] = hypgeo
-    } else {p_values[row] = NA}
-  }
-  return(p_values)
-}
-
-blastx_kegg_counts = cbind(blastx_kegg_counts, hypgeo_kegg(blastx_kegg_counts))
-colnames(blastx_kegg_counts)[6] = "p_value_hypgeo"
-blastx_kegg_counts = blastx_kegg_counts[order(blastx_kegg_counts$p_value_hypgeo),]
-blastx_kegg_counts = blastx_kegg_counts[which(blastx_kegg_counts$p_value_hypgeo < 0.05),]
-
-blastn_kegg_counts = cbind(blastn_kegg_counts, hypgeo_kegg(blastn_kegg_counts))
-colnames(blastn_kegg_counts)[6] = "p_value_hypgeo"
-blastn_kegg_counts = blastn_kegg_counts[order(blastn_kegg_counts$p_value_hypgeo),]
-blastn_kegg_counts = blastn_kegg_counts[which(blastn_kegg_counts$p_value_hypgeo < 0.05),]
-
-blastn_metasapp_kegg_counts = cbind(blastn_metasapp_kegg_counts, hypgeo_kegg(blastn_metasapp_kegg_counts))
-colnames(blastn_metasapp_kegg_counts)[6] = "p_value_hypgeo"
-blastn_metasapp_kegg_counts = blastn_metasapp_kegg_counts[order(blastn_metasapp_kegg_counts$p_value_hypgeo),]
-blastn_metasapp_kegg_counts = blastn_metasapp_kegg_counts[which(blastn_metasapp_kegg_counts$p_value_hypgeo < 0.05),]
