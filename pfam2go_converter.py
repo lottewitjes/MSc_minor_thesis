78cebfbfa794c6345628a7d09a5aa9c63c24c31f
lottewitjes <lottewitjes@outlook.com>
2018-06-18 15:14:20 +0200

    added pfam2go parser

--- pfam2go_converter.py
+++ pfam2go_converter.py
@@ -17,9 +17,33 @@ import sys
 import subprocess
 import os.path
 
+def parse_pfam2go(pfam2go_mapping):
+    dic = {}
+    with open(pfam2go_mapping, "r") as thefile:
+        for line in thefile:
+            if not line.startswith("!"):
+                pfam, go_term = line.strip().split(">")
+                pfam = pfam.split("Pfam:")[1].split(" ")[0]
+                go_term = go_term.split(";")[1].strip()
+                if pfam in dic:
+                    dic[pfam].append(go_term)
+                else:
+                    dic[pfam] = [go_term]
+    return dic
+
+def go_counter(pfam2go_dic, pfam_counts):
+    dic = {}
+    with open(pfam_counts, "r") as thefile:
+        for line in thefile:
+            pfam = line.strip().split("\t")[0]
+            
+
 if __name__ == "__main__":
     pfam2go = sys.argv[1]
     pfam_counts = sys.argv[2]
     go_counts = sys.argv[3]
 
+    pfam2go_dic = parse_pfam2go(pfam2go)
+
+    go_count_dic = go_counter(pfam2go_dic, pfam_counts)
 
