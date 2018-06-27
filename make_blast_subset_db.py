d8849d8c311e8f42e50b4afcecf3669d791d0ca0
lottewitjes <lottewitjes@outlook.com>
2018-06-27 12:38:27 +0200

    added subset db writer

--- make_blast_subset_db.py
+++ make_blast_subset_db.py
@@ -26,22 +26,35 @@ def blast_output_parser(blast_output):
             gene_protein = elements[1]
             bit_score = float(elements[11])
             e_value = float(elements[10])
-            if e_value <= 0.0000001 and bit_score >= 110: #set bitscore and e-value cutoff
-                if not gene_protein in blast_set
+            if e_value <= 0.0000001 and bit_score >= 74: #set bitscore and e-value cutoff
+                if not gene_protein in blast_set:
                     blast_set.add(gene_protein)
     return blast_set
+
+def write_subset_db(blast_total, gene_protein_db, subset_db):
+    with open(gene_protein_db, "r") as f1, open(subset_db, "w") as f2:
+        for line in f1:
+            gene_protein, sequence = line.strip().split(",")[0:2]
+            if gene_protein in blast_total:
+                header = ">{}".format(gene_protein)
+                f2.write(header + "\n" + sequence + "\n")
 
 if __name__ == "__main__":
     #Get variables from command line
     blast_result_dir = sys.argv[1]
     gene_protein_db = sys.argv[2]
+    subset_db = sys.argv[3]
 
+    #Make a set of genes/proteins from the BLASTN/X results
     blast_total = set()
     file_list = os.listdir(blast_result_dir)
     for file in file_list:
         if file.startswith("NG-5"):
             file_name = "".join([blast_result_dir, file])
+            print file_name
             blast_subset = blast_output_parser(file_name)
-            print len(blast_subset)
             blast_total = blast_total.union(blast_subset)
-    print len(blast_total)
+
+    #Make a subset db containing only those gene/protein sequences present in the BLASTN/X results
+    subset_db = "".join([blast_result_dir, subset_db])
+    write_subset_db(blast_total, gene_protein_db, subset_db)
